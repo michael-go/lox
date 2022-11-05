d8a2254e86dbaeed5ed25f65813de95c90571bcf
Michael Gokhman <michael.gokhman@yahoo.com>
2022-11-05 22:55:59 +0200

    test(rslox): include missing fixtures, fix error-sync testcase typo

--- rslox/tests/intergration.rs
+++ rslox/tests/intergration.rs
@@ -7,7 +7,9 @@ use test_case::test_case;
 // - https://crates.io/crates/test-generator
 #[test_case("basic")]
 #[test_case("unicode")]
-#[test_case("error-synchornize")]
+#[test_case("error-synchronize")]
+#[test_case("error-concat-not-string")]
+#[test_case("vars")]
 fn test_fixture(fname: &str) -> Result<()> {
     let lox_path = format!("tests/fixtures/{}.lox", fname);
 
@@ -20,16 +22,16 @@ fn test_fixture(fname: &str) -> Result<()> {
     let expected_out_path = format!("tests/fixtures/{}.out", fname);
     let expected_out = std::fs::read_to_string(expected_out_path)?;
     let re = Regex::new(
-        r"(?s)# exit code: (?P<ExitCode>\d+)\s*\n# stdout:\s*\n(?P<Stdout>.*)\n# stderr:\s*\n(?P<Stderr>.*)",
+        r"(?s)# exit code: (?P<ExitCode>\d+)\s*\n# stdout:\s*\n(?P<Stdout>.*)\n# stderr:\s*\n(?P<Stderr>.*)\n",
     )?;
     let caps = re.captures(&expected_out).unwrap();
     let exit_code = caps.name("ExitCode").unwrap().as_str().parse::<i32>()?;
     let stdout = caps.name("Stdout").unwrap().as_str();
     let stderr = caps.name("Stderr").unwrap().as_str();
 
-    assert_eq!(exit_code, output.status.code().unwrap());
-    assert_eq!(stdout, String::from_utf8(output.stdout)?);
-    assert_eq!(stderr, String::from_utf8(output.stderr)?);
+    assert_eq!(exit_code, output.status.code().unwrap(), "exit code");
+    assert_eq!(stdout, String::from_utf8(output.stdout)?, "stdout");
+    assert_eq!(stderr, String::from_utf8(output.stderr)?, "stderr");
 
     Ok(())
 }
