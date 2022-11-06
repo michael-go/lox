ce34a5dbff3fe3f0e8ecf88cc35a7ec366b2ffeb
Michael Gokhman <michael.gokhman@yahoo.com>
2022-11-06 20:57:29 +0200

    chore(rslox): use test-generator to pick fixtures directly from fs

--- rslox/tests/intergration.rs
+++ rslox/tests/intergration.rs
@@ -1,36 +1,36 @@
 use anyhow::Result;
 use regex::Regex;
-use test_case::test_case;
+use test_generator::test_resources;
 
-// TODO: to avoid manually listing the fixtures, can try to deduce from the directory via something like:
-// - https://mattjquinn.com/2020/autogen-paramaterized-rust-tests/
-// - https://crates.io/crates/test-generator
-#[test_case("basic")]
-#[test_case("unicode")]
-#[test_case("error-synchronize")]
-#[test_case("error-concat-not-string")]
-#[test_case("vars")]
-fn test_fixture(fname: &str) -> Result<()> {
-    let lox_path = format!("tests/fixtures/{}.lox", fname);
+#[test_resources("tests/fixtures/*.lox")]
+// we need this wrapper as test_generator doesn't expect a Result
+fn wrap_result(fname: &str) {
+    test_fixture(fname).unwrap()
+}
+
+fn test_fixture(lox_path: &str) -> Result<()> {
+    let expected_out_path = std::path::Path::new(lox_path).with_extension("out");
+    let expected_out = std::fs::read_to_string(expected_out_path)?;
+    let re = Regex::new(
+        r"(?s)# exit code: (?P<ExitCode>\d+)\s*\n# stdout:\s*\n(?P<Stdout>.*)\n# stderr:\s*\n(?P<Stderr>.*)\n",
+    )?;
+    let caps = re.captures(&expected_out).unwrap();
+    let expected_exit_code = caps.name("ExitCode").unwrap().as_str().parse::<i32>()?;
+    let expected_stdout = caps.name("Stdout").unwrap().as_str();
+    let expected_stderr = caps.name("Stderr").unwrap().as_str();
 
     let output = std::process::Command::new("target/debug/rslox")
         .args([lox_path])
         .output()?;
     println!("{:?}", output);
 
-    let expected_out_path = format!("tests/fixtures/{}.out", fname);
-    let expected_out = std::fs::read_to_string(expected_out_path)?;
-    let re = Regex::new(
-        r"(?s)# exit code: (?P<ExitCode>\d+)\s*\n# stdout:\s*\n(?P<Stdout>.*)\n# stderr:\s*\n(?P<Stderr>.*)\n",
-    )?;
-    let caps = re.captures(&expected_out).unwrap();
-    let exit_code = caps.name("ExitCode").unwrap().as_str().parse::<i32>()?;
-    let stdout = caps.name("Stdout").unwrap().as_str();
-    let stderr = caps.name("Stderr").unwrap().as_str();
-
-    assert_eq!(exit_code, output.status.code().unwrap(), "exit code");
-    assert_eq!(stdout, String::from_utf8(output.stdout)?, "stdout");
-    assert_eq!(stderr, String::from_utf8(output.stderr)?, "stderr");
+    assert_eq!(
+        expected_exit_code,
+        output.status.code().unwrap(),
+        "exit code"
+    );
+    assert_eq!(expected_stdout, String::from_utf8(output.stdout)?, "stdout");
+    assert_eq!(expected_stderr, String::from_utf8(output.stderr)?, "stderr");
 
     Ok(())
 }
